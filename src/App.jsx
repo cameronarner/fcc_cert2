328049523d9402bb01971e06fb7751e27dfe9866
Cameron Arner <carner@its.jnj.com>
2024-07-02 06:54:57 -0400

    added editor and preview boxes, currently renders the input from the editor as HMTL in the preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-
 import './App.css'
+import React, { Component } from "react";
+import { marked } from 'marked';
+
+class MarkdownPreviewer extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      input: "",
+      md: ""
+    };
+  }
+  handleChange(event){
+    this.setState({input: event.target.value, 
+    md: marked.parse(event.target.value)});
+  }
+  render(){
+    return(
+      <div>
+        <textarea id={"editor"} onChange={this.handleChange.bind(this)}></textarea>
+        <p id={"preview"}>{this.state.md}</p>
+      </div>
+    );
+  }
+}
+
 
 function App() {
   return (
-    <div>
-        <textarea id={"editor"}></textarea>
+    <div className="App">
+    < MarkdownPreviewer />
     </div>
   )
 }
-
 export default App
+
